ca9a2d640a19884ac52a44941a687bf0b57c5559
su-17 <su-17@wp.pl>
2019-11-28 10:59:02 +0100

    serializacja

--- portal_towarzyski/portal_towarzyski/portal/serializers.py
+++ portal_towarzyski/portal_towarzyski/portal/serializers.py
@@ -1,13 +1,26 @@
-from .models import zainteresowania_uzytkownika, Dane_uzytkownika
+from .models import Zainteresowania_uzytkownika, Dane_uzytkownika, Znajomi, Znajomi_has_zainteresowania_uzytkownik
 from rest_framework import serializers
 
-class zainteresowania_uzytkownika_Serializer(serializers.Serializer):
+class Zainteresowania_uzytkownika_Serializer(serializers.Serializer):
     class Meta:
-        model = zainteresowania_uzytkownika
+        model = Zainteresowania_uzytkownika
         fields = '__all__'
-
 
 class Dane_uzytkownika_Serializer(serializers.Serializer):
     class Meta:
         model = Dane_uzytkownika
         fields = '__all__'
+
+
+class Znajomi_Serializer(serializers.Serializer):
+    class Meta:
+        model = Znajomi
+        fields = '__all__'
+
+class Znajomi_has_zainteresowania_uzytkownik_Serializer(serializers.Serializer):
+    class Meta:
+        model = Znajomi_has_zainteresowania_uzytkownik
+        fields = '__all__'
+
+
+
